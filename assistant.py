39ada8b38d5f3c3b08ff2542930912a9107f79f6
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-08-14 18:10:04 -0700

    All category scores

--- assistant.py
+++ assistant.py
@@ -53,47 +53,18 @@ def get_message(run_status):
     
     return message
 
-# assistants = client.beta.assistants.list()
-# print(assistants)
-# exit()
-
-# removed after uploading files 
-# assistant = client.beta.assistants.create(
-#     name="Study Helper",
-#     model="gpt-3.5-turbo",
-#     instructions="You are a study partner for students who are new to technology.  When you answer prompts, do so with simple language suitable for someone learning fundamental concepts.",
-#     tools=[{"type": "file_search"}],
-# )
-
-# removed after creating and uploading files
-# create a vector store called "curriculum"
-# vector_store = client.beta.vector_stores.create(name="curriculum")
-
-# removed after completing
-# Ready the files for upload to OpenAI
-# file_paths = ["knowledge/CurriculumLessons4-7.pdf", "knowledge/OpenAIChatCompletionsAPICheatsheet.pdf"]
-# file_streams = [open(path, "rb") for path in file_paths]
-
-# removed after uploading
-# use the upload and poll sdk helpr to upload the files, add them to the vector store, 
-# and poll the status of the file batch for completion
-# file_batch = client.beta.vector_stores.file_batches.upload_and_poll(
-#   vector_store_id=vector_store.id, files=file_streams
-# )
-
-# removed after chekcing the status
-# print the status and the file counts of the batch to see the results of this operation
-# print(file_batch.status)
-# print(file_batch.file_counts)
-# exit()
+# function to check if category value is greater than threshold
+def check_category_scores(categories, threshold):
+    for key, value in categories:
+        if value > threshold:
+            return True
+        else:
+            return False  
 
 assistant = client.beta.assistants.update(
     assistant_id="asst_XdYTmnWKrnciClxxeCx9nGFI",
     tool_resources={"file_search": {"vector_store_ids": ["vs_eEtThd1XVzhYoVEFPdkh0HpG"]}}
 )
-
-# print(assistant)
-# exit()
 
 thread = client.beta.threads.create()
 
@@ -101,13 +72,9 @@ user_input = ""
 
 while True:
     if (user_input == ""):
-        # greet user with a print(), asking for their name and teling them how to end the session.
         print("Assistant: Hello there! Just so you know, you can type exit to end our chat. What's your name? ")
-        # prompt user for their name and assign it to a variable called name.
         name = input("You: ")
-        # greet the user with another print(), referencing their name.
         print("Assistant: Hey, " + name + "! How can I help you?")
-        # prompt the user for their question and assign that to user_input. this is the first request the assistant processes
         user_input = input("You: ")
     else:
         user_input = input("You: ")
@@ -121,22 +88,33 @@ while True:
         input = user_input
     )
    
-    # moderation threshold
-    # hate_threshold = 0.01
-    self_harm_threshold = .01
-
-    # Create/Use a while loop to check if the category score is more than the threshold set or is flagged
-    while moderation_result.results[0].category_scores.self_harm > self_harm_threshold or moderation_result.results[0].flagged == True:
-        print("Assistant: Sorry, your message violated our community guidelines.  Please try a different prompt.")
-        # moderate a new prompt
+#   initiate a loop as long as the category scores exceed a threshold of .7 or the content is flagged
+    while check_category_scores(moderation_result.results[0].category_scores, 0.7) or moderation_result.results[0].flagged == True:
+        print("Assistant: Sorry, your message violated our community guidelines. Please try a different prompt.")
         user_input = input("You: ")
-        # added code to exit in the loop
         if user_input.lower() == "exit":
             print("Goodbye!")
             exit()
         moderation_result = client.moderations.create(
             input = user_input
         )
+
+    # for checking single category, removed 
+    # set the threshold for single category
+    # self_harm_threshold = .01
+
+    # while loop to check if a single category score is more than the threshold set or is flagged
+    # while moderation_result.results[0].category_scores.self_harm > self_harm_threshold or moderation_result.results[0].flagged == True:
+    #     print("Assistant: Sorry, your message violated our community guidelines.  Please try a different prompt.")
+    #     # moderate a new prompt
+    #     user_input = input("You: ")
+    #     # added code to exit in the loop
+    #     if user_input.lower() == "exit":
+    #         print("Goodbye!")
+    #         exit()
+    #     moderation_result = client.moderations.create(
+    #         input = user_input
+    #     )
 
     message = client.beta.threads.messages.create(
         thread_id = thread.id,
